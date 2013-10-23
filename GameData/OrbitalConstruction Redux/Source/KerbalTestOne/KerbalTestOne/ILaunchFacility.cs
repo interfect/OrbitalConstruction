bf552a604778fd28bfc104348a8e6cc72d3b38f7
attosecond <big.laser@gmail.com>
2013-10-23 13:38:00 -0400

    Update ILaunchFacility.cs with new namespace name
    Changed namespace KerbalTestOne to OrbitalConstruction

--- GameData/OrbitalConstruction Redux/Source/KerbalTestOne/KerbalTestOne/ILaunchFacility.cs
+++ GameData/OrbitalConstruction Redux/Source/KerbalTestOne/KerbalTestOne/ILaunchFacility.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace KerbalTestOne
+namespace OrbitalConstruction
 {
     public interface ILaunchFacility: ISpaceDestination
     {
