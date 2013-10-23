7e89430f2364c231dd94e2f1d9fe5a8df6e6ff18
attosecond <big.laser@gmail.com>
2013-10-23 13:55:03 -0400

    Update SpaceDockUtilities18.cs namespace and code
    simpler search for spacedocks, and much simpler mass calculation

--- GameData/OrbitalConstruction Redux/Source/KerbalTestOne/KerbalTestOne/SpaceDockUtilities18.cs
+++ GameData/OrbitalConstruction Redux/Source/KerbalTestOne/KerbalTestOne/SpaceDockUtilities18.cs
@@ -1,25 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
 
-namespace KerbalTestOne
+namespace OrbitalConstruction
 {
     public static class SpaceDockUtilities18
     {
         public static bool DetermineIfVesselIsSpaceDock(Vessel v)
         {
+            //attosecond 10/23/13, there's a simpler way to do this (see below)
             foreach (Part p in v.parts)
             {
-                //see if it has our module
-                foreach (PartModule mod in p.Modules)
+                if (p.name.Equals("SpaceDock"))
                 {
-                    //MonoBehaviour.print(mod.moduleName);
-                    if (mod.moduleName.Equals("SpaceDock18"))
-                    {
+                    MonoBehaviour.print("Found a spacedock!");
                         return true;
-                    }
                 }
             }
             return false;
@@ -27,6 +24,7 @@ namespace KerbalTestOne
 
         public static List<Vessel> GetAllSpaceDocks(Vessel currentVessel)
         {
+            int cnt = 0;
             List<Vessel> docks = new List<Vessel>();
             foreach (Vessel v in FlightGlobals.Vessels)
             {
@@ -39,26 +37,20 @@ namespace KerbalTestOne
                 {
                     docks.Add(v);
                 }
+                else
+                {
+                    v.Unload();                 //added by attosecond 10/22/13 to reduce memory usage. The vessel gets loaded again if/when we need to check its RocketParts reserves.
+                }
             }
+            MonoBehaviour.print(FlightGlobals.Vessels.Count + " ships found. " + docks.Count + " are spacedocks.");
             return docks;
         }
 
 
         public static double DetermineMassOfVessel(Vessel v)
         {
-            double totalmass = 0;
-            foreach (Part p in v.parts)
-            {
-                MonoBehaviour.print("Part:" + p.name);
-                MonoBehaviour.print("Mass:" + p.mass);
-                totalmass += p.mass;
-                //we also need to coutn the resouces in each part, and multiply that by density... but that can wait.
-                double resMass = (double)p.GetResourceMass();
-                MonoBehaviour.print("ResMass:" + p.GetResourceMass());
-                totalmass += resMass;
-            }
-            MonoBehaviour.print("Mass of vessel: " + totalmass);
-            return totalmass;
+            return (double)v.GetTotalMass();
+            //entire function replaced by Vessel.GetTotalMass(), attosecond 10/22/13
         }
     }
 }
