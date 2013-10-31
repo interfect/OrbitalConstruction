96967102449b5a5cc2c67ab71734f05b54ed37bd
attosecond <big.laser@gmail.com>
2013-10-31 01:21:23 -0400

    Update AssemblyInfo.cs

--- GameData/OrbitalConstruction Redux/Source/KerbalTestOne/KerbalTestOne/Properties/AssemblyInfo.cs
+++ GameData/OrbitalConstruction Redux/Source/KerbalTestOne/KerbalTestOne/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrbitalConstruction")]
+[assembly: AssemblyTitle("OrbitalManufacturing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrbitalConstruction")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("OrbitalManufacturing")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a797cd5-4cc8-4141-9771-de0cdbfa3753")]
+[assembly: Guid("ac57ec5b-5de4-469e-9d99-78bfd89537ec")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
